51adfe4a842d536875c12b5bc3e1ca5d00acc66b
vasim1234 <vasimaiyub@gmail.com>
2026-09-21 18:02:14 +0530

    Update application ID and namespace in build.gradle.kts

--- smart_calculator/android/app/build.gradle.kts
+++ smart_calculator/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.s_one.smartcalculator"
+    namespace = "com.vasim1234.smartcalculator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.s_one.smartcalculator"
+applicationId = "com.vasim1234.smartcalculator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
