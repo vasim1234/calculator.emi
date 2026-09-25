f8791914eda0ed0a3ca58b1f4adfb38cd3dc46ff
vasim1234 <vasimaiyub@gmail.com>
2026-09-25 09:49:42 +0530

    Remove unused media_store_plus import

--- smart_calculator/lib/locker_utils.dart
+++ smart_calculator/lib/locker_utils.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:media_store_plus/media_store_plus.dart';
 
 class LockerUtils {
   static const String _pinKey = 'locker_pin';
